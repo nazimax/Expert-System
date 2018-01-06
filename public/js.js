8626adf5870205e247f128ae998577ba17da0572
nazimax <kissi.salim.yahia@outlook.fr>
2018-01-06 12:33:07 +0100

    adding result of diagnostiq with some styling

--- public/js.js
+++ public/js.js
@@ -7,16 +7,30 @@ Array.prototype.remove = function(from, to) {
 
 var myVar;
 var list=[];
+var sign=[];
 function myFunction() {
-    document.getElementById("hypos").innerHTML="<div></div>";
+    document.getElementById("hypos").style.display="none";
+
     document.getElementById("loader").style.display="block";
-    myVar = setTimeout(showPage, 2000);
+    myVar = setTimeout(showPage, 1200);
+
+}
+
+
+function showingSignes() {
+    document.getElementById("sign14").style.visibility="hidden";
+    // console.log(document.getElementById("sign14"))
+
 }
 
 function showPage() {
     document.getElementById("loader").style.display = "none";
-    document.getElementById("myDiv").style.display = "block";
+    signes();
 
+
+
+    // document.getElementById("myDiv").innerHTML=buildHtml()
+    // document.getElementById("myDiv").style.display = "block";
 
 }
 
@@ -45,18 +59,83 @@ function clearList() {
 
 }
 
+function switchArray(element) {
+
+    switch (element){
+        case "checkbox1" :
+        case "checkbox2" :
+        case "checkbox3" :
+        case "checkbox4" :
+        {
+
+            if(sign.indexOf("Entorse")===-1){
+
+                sign.push("Fracture non déplacée",
+                    "Fracture déplacée","Entorse",
+                    "Luxation")
+
+            }
+
+
+            break;
+        }
+        case "checkbox5":
+        case "checkbox6":
+        case "checkbox7":
+        case "checkbox8":
+        {
+
+            if(sign.indexOf("Fracture vertébrale")===-1) {
+                sign.push("Fracture vertébrale")
+            }
+            break;
+        }
+        case "checkbox9":
+        case "checkbox10":
+        case "checkbox11":
+        case "checkbox12":
+        case "checkbox13":
+        {
+            if(sign.indexOf("Traumatisme cranien")===-1) {
+                sign.push("Traumatisme cranien")
+            }
+            break;
+        }
+    }
+}
+
 
 function signes() {
-    signe_1_4=["checkbox1","checkbox2","checkbox3","checkbox4"]
 
-    var sign=[]
     list.forEach(function (e) {
-
-
+        switchArray(e)
     })
-
-
-
-
+    console.log(sign)
+    localStorage.setItem("SIGN",sign)
+    location.href="signes.html";
 
 }
+
+
+function buildHtml() {
+    var counter=15;
+    var content="";
+
+    var div='<div class="funkyradio-warning"> <input type="checkbox" name="checkbox" id="';
+    var div2=   'onchange="handleChange(this)"/> <label for="' ;
+
+    var tag=""
+
+    sign.forEach(function (e) {
+
+        var div3 =   '>'+e+'</label> </div>';
+
+        var id="checkbox"+counter+"\" "
+        tag+=div+id+div2+id+div3
+        counter++;
+
+    })
+    return tag
+}
+
+
--- public/test.js
+++ public/test.js
@@ -1,4 +1,5 @@
 function switchArray(element) {
+    sign=["Traumatisme crànien"]
     switch (element){
         case "checkbox1" :
         case "checkbox2" :
@@ -6,6 +7,13 @@ function switchArray(element) {
         case "checkbox4" :
         {
 
+            if(sign.indexOf("Entorse")===-1){
+
+                sign.push("Fracture non déplacée",
+                    "Fracture déplacée","Entorse",
+                    "Luxation")
+
+            }
 
 
             break;
@@ -16,7 +24,9 @@ function switchArray(element) {
         case "checkbox8":
         {
 
-
+            if(sign.indexOf("Fracture vertébrale")===-1) {
+                sign.push("Fracture vertébrale")
+            }
             break;
         }
         case "checkbox9":
@@ -25,8 +35,13 @@ function switchArray(element) {
         case "checkbox12":
         case "checkbox13":
         {
-
-
+            if(sign.indexOf("Traumatisme crànien")===-1) {
+                sign.push("Traumatisme crànien")
+                console.log('c bon')
+            }
+            else {
+                console.log('else')
+            }
 
             break;
         }
@@ -34,6 +49,33 @@ function switchArray(element) {
 
 
     }
+
+
 }
 
-switchArray("checkbox5");
+
+
+
+var sign=["NAZIM","AYMEN","MOKHTAR","ABDELJALIL"]
+
+function buildHtml() {
+    var counter=15;
+    var content="";
+
+    var div='<div class="funkyradio-warning"> <input type="checkbox" name="checkbox" id="';
+    var div2=   'onchange="handleChange(this)"/> <label for="' ;
+
+    var tag=""
+
+    sign.forEach(function (e) {
+
+        var div3 =   '>'+e+'</label> </div>';
+
+        var id="checkbox"+counter+"\" "
+        tag+=div+id+div2+id+div3
+        counter++;
+
+    })
+    return tag
+}
+console.log(buildHtml())
